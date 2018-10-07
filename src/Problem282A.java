a4e114559eace0b05bfb55c4ab5b33a5220814b3
Suneet Srivastava <suneetbond91@gmail.com>
2018-10-07 13:36:59 +0530

    Problem282A

---
+++ src/Problem282A.java
@@ -0,0 +1,33 @@
+/**
+ * @author : codedsun
+ * Created on 07/10/18
+ */
+
+import java.util.Scanner;
+
+/**
+ * <a href="http://codeforces.com/problemset/problem/282/A"/>
+ */
+public class Problem282A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // the no of statements
+        int x = 0; //
+        while (n-- >= 1) {
+            String operation = sc.next();
+            if (operation.charAt(0) == 'X') {
+                if (operation.charAt(1) == '+') {
+                    x++;
+                }
+                if (operation.charAt(1) == '-') {
+                    x--;
+                }
+            } else if (operation.charAt(0) == '+') {
+                ++x;
+            } else {
+                --x;
+            }
+        }
+        System.out.println(x);
+    }
+}
