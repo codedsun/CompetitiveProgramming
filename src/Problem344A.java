adffb9ab77e9b5b6807dafb0b96013eab790d5a7
Suneet Srivastava <suneetbond91@gmail.com>
2019-01-04 01:53:22 +0530

    Problem 344A

---
+++ src/Problem344A.java
@@ -0,0 +1,29 @@
+/**
+ * @author : codedsun
+ * Created on 04/01/19
+ */
+
+import java.util.Scanner;
+
+/**
+ * <a href="https://codeforces.com/problemset/problem/344/A"/>
+ */
+public class Problem344A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();//the no of magnets
+        String s, s1 = "";
+        int count = 0;
+        while (n-- >= 1) {
+            s = sc.next();
+            if (s.equals("10") && (!(s.equals(s1)))) {
+                count++;
+                s1 = "10";
+            } else if (s.equals("01") && ((!(s.equals(s1))))) {
+                count++;
+                s1 = "01";
+            }
+        }
+        System.out.println(count);
+    }
+}
