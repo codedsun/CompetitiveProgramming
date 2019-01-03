8842b35f91bd40191b74e7bc039d784d28270ca0
Suneet Srivastava <suneetbond91@gmail.com>
2019-01-04 00:16:15 +0530

    Problem 318A

---
+++ src/Problem318A.java
@@ -0,0 +1,33 @@
+/**
+ * @author : codedsun
+ * Created on 04/01/19
+ */
+
+import java.util.Scanner;
+
+/**
+ * <a href="https://codeforces.com/problemset/problem/318/A"/>
+ */
+public class Problem318A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+        //Calculate total Odd terms in the series
+        long oddTerms = 0;
+        if (n % 2 == 0) {
+            //n = even
+            oddTerms = (((n - 1 - 1) / 2) + 1);
+        } else {
+            // n = odd
+            oddTerms = (((n - 1) / 2) + 1);
+        }
+
+        if (k > oddTerms) {
+            k = k - oddTerms;
+            System.out.println(k * 2);
+            return;
+        }
+        System.out.println((k * 2) - 1);
+    }
+}
