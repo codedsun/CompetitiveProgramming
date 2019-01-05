43e275ed14055d96956a80e537ab181fd66ee994
Suneet Srivastava <suneetbond91@gmail.com>
2019-01-06 01:47:42 +0530

    Problem 469A

---
+++ src/Problem469A.java
@@ -0,0 +1,33 @@
+/**
+ * @author : codedsun
+ * Created on 06/01/19
+ */
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * <a href="https://codeforces.com/problemset/problem/469/A"/a>
+ */
+public class Problem469A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p = sc.nextInt();
+        Set<Integer> distinctElements = new LinkedHashSet<>();
+        while (p-- >= 1) {
+            distinctElements.add(sc.nextInt());
+        }
+        int q = sc.nextInt();
+        while (q-- >= 1) {
+            distinctElements.add(sc.nextInt());
+        }
+        if (distinctElements.size() == n) {
+            System.out.println("I become the guy.");
+        } else {
+            System.out.println("Oh, my keyboard!");
+        }
+
+    }
+}
