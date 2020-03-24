585efa78eaf74bfccc440f36c66fa1a3949459da
codedsun <suneetbond91@gmail.com>
2020-03-25 01:03:01 +0530

    Problem 766B

--- src/Codeforces/Problem1206B.java
+++ src/Codeforces/Problem1206B.java
@@ -1,44 +1,41 @@
 package Codeforces;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Problem1206B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long a[] = new long[n];
-        List<Long> minus = new ArrayList<>();
-        List<Long> plus = new ArrayList<>();
-        List<Long> zeros = new ArrayList<>();
-        long count =0;
+        long a;
+        ArrayList<Long> minus = new ArrayList<>();
+        ArrayList<Long> plus = new ArrayList<>();
+        ArrayList<Long> zero = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextLong();
-            if(a[i]<0){
-                minus.add(a[i]);
-            }else if(a[i]==0){
-                zeros.add(a[i]);
-            }else{
-                plus.add(a[i]);
+            a = sc.nextLong();
+            if (a < 0) {
+                minus.add(a);
+            } else if (a == 0) {
+                zero.add(a);
+            } else {
+                plus.add(a);
             }
         }
-        if(minus.size()%2==0){
-            for(int i=0; i<minus.size();i++){
-                count = count+ -1-(minus.get(i));
-            }
-        }else{
-            for(int i=0; i<minus.size()-1;i++){
-                count = count+ -1-(minus.get(i));
-            }
-            count = count+1-minus.get(minus.size()-1);
+        a = 0;
+        for (int j = 0; j < plus.size(); j++) {
+            a = a + (plus.get(j) - 1);
         }
-        count += (zeros.size());
-        for(int i=0; i<plus.size();i++){
-            count+= plus.get(i)-1;
+
+        for (int k = 0; k < minus.size(); k++) {
+            a = a + (-1 - minus.get(k));
+
         }
-        System.out.println(count);
+        if(zero.size()>0) {
+            a = a + zero.size();
+        }else if(minus.size()%2==1){
+            a = a+2;
+        }
+        System.out.println(a);
 
     }
 }
--- src/Codeforces/Problem766B.java
+++ src/Codeforces/Problem766B.java
@@ -1,11 +1,34 @@
 package Codeforces;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * <a href="https://codeforces.com/problemset/problem/766/B"/>
+ */
 public class Problem766B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0; i<n;i++){
+            a[i] = sc.nextInt();
+        }
+
+        Arrays.sort(a);
+        int yes = 0;
+        for(int i=2; i<n; i++){
+            if(a[i-1]+a[i-2]>a[i]){
+                yes=1;
+                break;
+            }
+        }
+        if(yes == 1){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+
 
     }
 }
