c88784a500e4970bb04ee37ec29a2e82a7d97006
codedsun <suneetbond91@gmail.com>
2020-08-10 07:27:07 +0530

    feat: Updated gitignore

--- src/Suneet.java
+++ src/Suneet.java
@@ -82,77 +82,89 @@ import java.util.*;
 //}
 
 
+//public class Suneet {
+//
+//    public static class Graph{
+//        int vertices;
+//        LinkedList<Integer> adjList[];
+//
+//        public Graph(int vertices) {
+//            this.vertices = vertices;
+//            adjList = new LinkedList[vertices];
+//            for(int i =0; i<vertices; i++){
+//                adjList[i] = new LinkedList<>();
+//            }
+//        }
+//
+//        void addEdge(int v, int w) {
+//            adjList[v].add(w);
+//        }
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int k = sc.nextInt();
+//        Graph graph = new Graph(n);
+//        int cats[] = new int[n];
+//        for(int i=0; i<n; i++) {
+//            cats[i] = sc.nextInt();
+//        }
+//        for(int j=0;j<n-1;j++) {
+//            graph.addEdge(sc.nextInt(), sc.nextInt());
+//        }
+//
+////        printGraph(graph);
+//
+//        DFS(graph.vertices, graph);
+//    }
+//
+//    static void DFS(int v, Graph graph)
+//    {
+//        // Mark all the vertices as not visited(set as
+//        // false by default in java)
+//        boolean visited[] = new boolean[v];
+//
+//        // Call the recursive helper function to print DFS traversal
+//        DFSUtil(v-1, visited, graph);
+//    }
+//    private static void printGraph(Graph graph)
+//    {
+//        for(int v = 0; v < graph.vertices; v++)
+//        {
+//            System.out.println("Adjacency list of vertex "+ v);
+//            System.out.print("head");
+//            for(Integer pCrawl: graph.adjList[v]){
+//                System.out.print(" -> "+pCrawl);
+//            }
+//            System.out.println("\n");
+//        }
+//    }
+//
+//    private static void DFSUtil(int v,boolean visited[], Graph graph)
+//    {
+//        // Mark the current node as visited and print it
+//        visited[v] = true;
+//        System.out.print(v+" ");
+//
+//        // Recur for all the vertices adjacent to this vertex
+//        Iterator<Integer> i = graph.adjList[v].listIterator();
+//        while (i.hasNext())
+//        {
+//            int n = i.next();
+//            if (!visited[n])
+//                DFSUtil(n, visited, graph);
+//        }
+//    }
+//}
+
 public class Suneet {
-
-    public static class Graph{
-        int vertices;
-        LinkedList<Integer> adjList[];
-
-        public Graph(int vertices) {
-            this.vertices = vertices;
-            adjList = new LinkedList[vertices];
-            for(int i =0; i<vertices; i++){
-                adjList[i] = new LinkedList<>();
-            }
-        }
-
-        void addEdge(int v, int w) {
-            adjList[v].add(w);
-        }
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        Graph graph = new Graph(n);
-        int cats[] = new int[n];
-        for(int i=0; i<n; i++) {
-            cats[i] = sc.nextInt();
-        }
-        for(int j=0;j<n-1;j++) {
-            graph.addEdge(sc.nextInt(), sc.nextInt());
-        }
-
-//        printGraph(graph);
-
-        DFS(graph.vertices, graph);
-    }
-
-    static void DFS(int v, Graph graph)
-    {
-        // Mark all the vertices as not visited(set as
-        // false by default in java)
-        boolean visited[] = new boolean[v];
-
-        // Call the recursive helper function to print DFS traversal
-        DFSUtil(v-1, visited, graph);
-    }
-    private static void printGraph(Graph graph)
-    {
-        for(int v = 0; v < graph.vertices; v++)
-        {
-            System.out.println("Adjacency list of vertex "+ v);
-            System.out.print("head");
-            for(Integer pCrawl: graph.adjList[v]){
-                System.out.print(" -> "+pCrawl);
-            }
-            System.out.println("\n");
-        }
-    }
-
-    private static void DFSUtil(int v,boolean visited[], Graph graph)
-    {
-        // Mark the current node as visited and print it
-        visited[v] = true;
-        System.out.print(v+" ");
-
-        // Recur for all the vertices adjacent to this vertex
-        Iterator<Integer> i = graph.adjList[v].listIterator();
-        while (i.hasNext())
-        {
-            int n = i.next();
-            if (!visited[n])
-                DFSUtil(n, visited, graph);
-        }
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        String s = sc.nextLine();
+//        long na = (long) Math.pow(1,2);
+//        Math.log()
+//        StringBuilder ss = new StringBuilder(s);
+//        System.out.println(ss.reverse().toString());
     }
 }
