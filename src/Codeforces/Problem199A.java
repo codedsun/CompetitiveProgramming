489090ae8c4b11c230e654aed14b317017c4a983
Suneet Srivastava <suneetbond91@gmail.comm>
2021-06-11 11:56:52 +0530

    Problem 199A

---
+++ src/Codeforces/Problem199A.java
@@ -0,0 +1,11 @@
+package Codeforces;
+
+import java.util.Scanner;
+
+public class Problem199A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("0 0 "+n);
+    }
+}
